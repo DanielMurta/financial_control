75fbf554eeae36779a7607353b6051057711ce43
Daniel Murta <murtdaniel@gmail.com>
2025-09-26 11:46:30 -0300

    Add unique index on user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   has_many :accounts, dependent: :destroy
 end
--- db/migrate/20240929041703_create_users.rb
+++ db/migrate/20240929041703_create_users.rb
@@ -3,9 +3,11 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name 
       t.string :last_name
-      t.string :email, null: false, unique: true
+      t.string :email, null: false
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end

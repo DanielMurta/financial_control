e1bd0149feadfb9c09f013238eca5395a3ff233b
Daniel Murta <murtdaniel@gmail.com>
2025-09-26 11:46:50 -0300

    Add update action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,10 +21,30 @@ module Api
         end
       end
 
+      def update
+        user = User.find_by(id: params[:id])
+
+        render_not_found and return unless user
+
+        if user.update(user_update_params)
+          render json: { data: user, message: 'User updated successfully' }, status: :ok
+        else
+          render json: { data: user.errors, message: 'User not updated' }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def user_params
         params.permit(:first_name, :last_name, :email)
+      end
+
+      def user_update_params
+        params.permit(:first_name, :last_name)
+      end
+
+      def render_not_found
+        render json: { data: {}, message: 'User not found' }, status: :not_found
       end
     end
   end

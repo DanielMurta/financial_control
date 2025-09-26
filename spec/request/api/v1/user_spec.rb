56c814657707eb754c4c946ac0219d3b0c3ae5e6
Daniel Murta <murtdaniel@gmail.com>
2025-09-26 11:47:23 -0300

    Add more create context and update specs

--- spec/request/api/v1/user_spec.rb
+++ spec/request/api/v1/user_spec.rb
@@ -59,6 +59,33 @@ RSpec.describe 'Api::V1::Users', type: :request do
         expect(json_response['data']).to include('email' => ["can't be blank"])
       end
     end
+
+    context 'when the email is not unique' do
+      subject(:make_request) do
+        post api_v1_users_path, params: { first_name: 'Daniel', last_name: 'Murta', email: user.email }
+      end
+
+      let(:user) { create(:user) }
+
+      it 'does not create a new user' do
+        make_request
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the errors' do
+        make_request
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['data']).to include('email' => ['has already been taken'])
+      end
+
+      it 'return the correct message' do
+        make_request
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['message']).to eq('User not created')
+      end
+    end
   end
 
   describe '#show' do
@@ -85,6 +112,65 @@ RSpec.describe 'Api::V1::Users', type: :request do
     context 'when the user does not exist' do
       subject(:make_request) do
         get api_v1_user_path(id: 'non-existent-id')
+      end
+
+      it 'return the correct status' do
+        make_request
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'return the correct message' do
+        make_request
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['message']).to eq('User not found')
+      end
+
+      it 'returns an empty data' do
+        make_request
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['data']).to be_empty
+      end
+    end
+  end
+
+  describe '#update' do
+    subject(:make_request) do
+      put api_v1_user_path(user), params: { first_name: 'New first name', last_name: 'New last name' }
+    end
+
+    let(:user) { create(:user) }
+
+    it 'updates the user' do
+      make_request
+      expect(user.reload.first_name).to eq('New first name')
+    end
+
+    it 'return the correct status' do
+      make_request
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns the updated user' do
+      make_request
+      json_response = JSON.parse(response.body)
+
+      expect(json_response['data']).to include(
+        'first_name' => 'New first name', 'last_name' => 'New last name', 'email' => user.email
+      )
+    end
+
+    it 'return the correct message' do
+      make_request
+      json_response = JSON.parse(response.body)
+
+      expect(json_response['message']).to eq('User updated successfully')
+    end
+
+    context 'when the user does not exist' do
+      subject(:make_request) do
+        put api_v1_user_path(id: 'non-existent-id'), params: { first_name: 'first name', last_name: 'last name' }
       end
 
       it 'return the correct status' do
